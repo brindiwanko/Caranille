46a3150fdac6dc6ac5b0a7d7df65eb6dfdb268e1
Caranille <caranille@outlook.fr>
2025-07-15 12:05:29 +0200

    fix

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 $config = 'kernel/config.php';
-$size = filesize($config);
 
 //Si le fichier existe on redirige le joueur vers le jeu
 if (file_exists($config)) 
 {
 	header("Location: modules/main/index.php");
+	exit();
 }
 //Sinon on redirige l'utilisateur vers l'accueil du site
 else
 {
 	header("Location: kernel/install/modules/step-1.php");
+	exit();
 }

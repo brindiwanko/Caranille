370d49d91fab4b21e1e0de9054da1175097e07dd
brindiwanko <mycaranille@gmail.com>
2025-11-15 09:35:17 +0100

    Fix comments in PasswordManager class

--- kernel/security/passwordManager.php
+++ kernel/security/passwordManager.php
@@ -2,12 +2,12 @@
 
 class PasswordManager {
 
-    //On hash le mot de passe soumit avec bcrypt
+    //On hash le mot de passe soumis avec bcrypt
     public static function hashPassword($password) {
         return password_hash($password, PASSWORD_DEFAULT);
     }
 
-    //On vérifie si le mot de passe soumit est le même que celui en base de données
+    //On vérifie si le mot de passe soumis est le même que celui en base de données
     public static function verifyPassword($password, $hash) {
         return password_verify($password, $hash);
     }

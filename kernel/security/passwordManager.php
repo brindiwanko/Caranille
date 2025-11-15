66ad5a998168c58ec4368b1aaa7a353d38697ad5
brindiwanko <mycaranille@gmail.com>
2025-11-15 09:30:19 +0100

    Add comments for password hashing and verification
    Added comments in French to clarify password hashing and verification methods.

--- kernel/security/passwordManager.php
+++ kernel/security/passwordManager.php
@@ -1,11 +1,13 @@
 <?php
 
 class PasswordManager {
-    
+
+    //On hash le mot de passe avec bcrypt
     public static function hashPassword($password) {
         return password_hash($password, PASSWORD_DEFAULT);
     }
-    
+
+    //On vérifie si le mot de passe soumit est le même que celui en base de données
     public static function verifyPassword($password, $hash) {
         return password_verify($password, $hash);
     }
